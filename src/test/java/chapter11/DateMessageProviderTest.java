237fe65044a8514eb4fba9ba2c2ab6a815ce6195
simjunbo <s860620@gmail.com>
2019-06-04 20:38:27 +0900

    chapter11

---
+++ src/test/java/chapter11/DateMessageProviderTest.java
@@ -0,0 +1,28 @@
+package chapter11;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+
+import static org.junit.Assert.assertEquals;
+
+public class DateMessageProviderTest {
+
+	@Test
+	public void 오전() throws Exception {
+		DateMessageProvider provider = new DateMessageProvider();
+		assertEquals("오전", provider.getDateMessage(creteCurrentDate(11)));
+	}
+
+	@Test
+	public void 오후() throws Exception {
+		DateMessageProvider provider = new DateMessageProvider();
+		assertEquals("오후", provider.getDateMessage(creteCurrentDate(13)));
+	}
+
+	private Calendar creteCurrentDate(int hourOfDay) {
+		Calendar now = Calendar.getInstance();
+		now.set(Calendar.HOUR_OF_DAY, hourOfDay);
+		return now;
+	}
+}
